b13be02c1bc15120a4e63685224e4ca875fc6a6b
Ashish_Upadhyay_git01 <161508428+Ashishupadhyaygit0001@users.noreply.github.com>
2024-06-23 21:36:45 +0530

    Update Addersub.v

--- Addersub.v
+++ Addersub.v
@@ -1,37 +1,24 @@
 `timescale 1ns / 1ps
 
 
-module Addersub(
-input[15:0]a,b,
-input M,
-output reg[15:0] s,
-output reg co);
+module Addersub( input[15:0]a,b, input M, output reg[15:0] s, output reg co);
 
 
 always @(*)
-begin
-// for the sum of 2 numbers 
-if(M==0)
+  begin
+  if(M==0)
+    begin
+      {co,s}=a+b+16'b0000000000000000;
+    end  
+    else if(M==1)
+      begin
+        {co,s}=a+(16'b1111111111111111^b)+16'b0000000000000001;
+        if(co==0)
+          begin
+            {co,s}=(16'b1111111111111111^s)+16'b0000000000000000+16'b0000000000000001;
+          end
+      end
 
-begin
-{co,s}=a+b+16'b0000000000000000;
-end
-  
-// for the subtraction of 2 number
-else if(M==1)
-
-begin
-{co,s}=a+(16'b1111111111111111^b)+16'b0000000000000001;
-  
-// to get the number in 2's complement
-if(co==0)
-
-begin
-{co,s}=(16'b1111111111111111^s)+16'b0000000000000000+16'b0000000000000001;
-end
-
-end
-
-end
+  end
 
 endmodule
